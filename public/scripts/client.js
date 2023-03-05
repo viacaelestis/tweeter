4a5c217a8c5630f0a76b3f28503387f33a11a1f8
viacaelestis <affectuminflamma@gmail.com>
2023-03-05 03:19:37 -0700

    make page refresh when submit button is pressed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,7 +17,7 @@ const createTweetElement = (tweetData) => {
   const $flagIcon = $('<i>').addClass('icon fas fa-flag');
   const $retweetIcon = $('<i>').addClass('icon fas fa-retweet');
   const $likeIcon = $('<i>').addClass('icon fas fa-heart');
-  
+
   $header.append($avatar).append($name).append($handle);
   $body.append($text);
   $footer.append($timestamp).append($flagIcon).append($retweetIcon).append($likeIcon);
@@ -26,73 +26,89 @@ const createTweetElement = (tweetData) => {
   return $tweet;
 }
 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
+const renderTweets = (tweets) => {
+  const $tweetList = $('#tweets-list');
+  $tweetList.empty();
+  for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
     $('#tweet-container').append($tweet);
   }
 }
 
-// Form submission using JQuery
-$(document).ready(function() {
-  // Get the error element and hide it
-  const $error = $('#error').hide();
+// fetch tweets from /tweets page
+const loadTweets = () => {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      // Sort the tweets by their creation time in descending order
+      data.sort((a, b) => new Date(b.created_at - new Date(a.created_at)));
+      // Render the tweets to the page
+      renderTweets(data);
+    },
+    error: function(error) {
+      console.log('Error:', error);
+    }
+  });
+}
 
-  // listen for form submit event
+
+$(document).ready(() => {
+  // Get the error element and hide it
+  const $error = $('#error-message').hide();
+
+  // Add event listener for form submission
   $('#tweet-form').submit(function(event) {
     // prevent default form submission behavior
     event.preventDefault();
-    
-    // serialize form data
-    const formData = $(this).serialize();
-    
-    // send data as POST request to server
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: formData
-    })
-    .then(function(response) {
-      // handle successful response from server
-      console.log(response);
-      // load tweets again to update the page with the new tweet
-      loadTweets();
-      // hide the error message if it is currently visible
-      $error.slideUp();
-    })
-    .catch(function(error) {
-      // handle error response from server
-      console.log(error);
-      // get the error message from the server response
-      const errorMessage = error.responseJSON.error;
-      // set the text of the error element to the error message and show it
-      $error.text(errorMessage).slideDown();
-    });
+
+    const tweetText = $('#tweet-text').val().trim(); // Remove leading/trailing whitespace
+
+    // Check if tweet is empty or too long
+    if (tweetText === "" || tweetText.length > 140) {
+      $error.text("Invalid tweet. Please enter 1-140 characters.").slideDown(); // Display error message
+    } else {
+      // serialize form data
+      const formData = $(this).serialize();
+
+      // send data as POST request to server
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: formData
+      })
+      .then(function(response) {
+        // handle successful response from server
+        console.log(response);
+        // load tweets again to update the page with the new tweet
+        loadTweets();
+        // hide the error message if it is currently visible
+        $error.slideUp();
+      })
+      .catch(function(error) {
+        // handle error response from server
+        console.log(error);
+        // get the error message from the server response
+        const errorMessage = error.responseJSON.error;
+        // set the text of the error element to the error message and show it
+        $error.text(errorMessage).slideDown();
+      });
+      
+      // Clear textarea and reset character counter
+      $('#tweet-text').val("");
+      $('.counter').text("140");
+    }
   });
-});
-
-// fetch tweets from /tweets page
-$(document).ready(function() {
-
-  function loadTweets() {
-    $.ajax({
-      url: '/tweets',
-      method: 'GET',
-      dataType: 'json',
-      success: function(data) {
-        console.log(data);
-        // Sort the tweets by their creation time in descending order
-        data.sort((a, b) => new Date(b.created_at - new Date(a.created_at)));
-        // Render the tweets to the page
-        renderTweets(data);
-      },
-      error: function(error) {
-        console.log('Error:', error);
-      }
-    });
-  }
 
   // Call loadTweets to load tweets on page load
   loadTweets();
+});
 
+$(document).ready(function() {
+  // Add event listener for refresh button
+  $('#refresh-button').click(function() {
+    location.reload(); // Reload the current page
+  });
 });
